1dc83d340b094e89e108a68e236e1f72003586e8
Shuhe Kurita <kurita@nlp.ist.i.kyoto-u.ac.jp>
2020-02-18 21:23:04 -0500

    move files..

--- sshjob/__init__.py
+++ sshjob/__init__.py
@@ -1,6 +1,6 @@
-from pyraiden.shell_runs import *
-from pyraiden.job_classes import *
-from pyraiden.main import *
-from pyraiden.supports import *
-from pyraiden.track import *
+from sshjob.sshjob import *
+from sshjob.shell_runs import *
+from sshjob.track import *
+#from sshjob.job_classes import *
+#from sshjob.supports import *
 
