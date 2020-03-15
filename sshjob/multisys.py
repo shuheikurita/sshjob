6ad30e54c60dc9f78bcb40070234e3783fb5b7f6
Shuhe Kurita <kurita@nlp.ist.i.kyoto-u.ac.jp>
2020-03-15 02:10:27 -0400

    sshjob for single system environment

---
+++ sshjob/multisys.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# Author: Shuhei Kurita
+# Mailto: kurita@nlp.ist.i.kyoto-u.ac.jp
+# Licence: GPL v2
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import datetime
+#from datetime import datetime, timedelta, timezone
+import re
+import json
+from time import sleep
+
+from collections import OrderedDict
+
+from sshjob.shell_runs import *
+from sshjob.job_queues import *
+from sshjob.sshjob import *
+
+class sshjob(OrderedDict):
+    @staticmethod
+    def version():
+        return '0.1.1'
+    def __init__(self,
+                 envs={"local":":::SHELL"},
+                 job_queues={"SHELL":shell},
+                 file=None):
+        super(sshjob, self).__init__()
+        assert isinstance(envs,dict)
+        assert isinstance(job_queues,dict)
+        for label,env in envs.items():
+            self[label]=sshjobsys(env,job_queues)
+        self.job_queues = job_queues
+        if file:
+            self.load(file)
+        elif file=="":
+            if file:
+                self.load(file)
+                print("[SSHJOB] load from "+self.file)
+            else:
+                date=datetime.datetime.today().strftime("%Y%m%d")[2:]
+                file = "pyjobs"+"."+date
+                for i in range(100):
+                    if not os.path.isfile(file+".%02d"%i):
+                        self.file = file+".%02d"%i
+                        print("[SSHJOB] Save to",self.file)
+                        print("[SSHJOB] For loading, use:  jobs.load(\"%s\")"%self.file)
+                        break
+                else:
+                    raise NameError("[SSHJOB] TOO MANY FILES")
+        self.job_queues = DEFAULT_JOB_QUEUE
+        self.add_job_queue(job_queues)
+
+    @property
+    def __dict__(self):
+        expand={"file":self.file, "sshjob":{}}
+        for label,jobsys in self.items():
+            if isinstance(label,str):
+                expand["sshjob"][label]=jobsys.__dict__
+        return {"sshjob":expand}
+    def show(self,**kwargs):
+        for label,jobsys in self.items():
+            if isinstance(label,str):
+                print("# "+label)
+                jobsys.show(**kwargs)
+    def qsub(self,env,**kwargs):
+        if env not in self:
+            print("env="+env+" not found.")
+        else:
+            self[env].qsub(**kwargs)
+
+    def dump(self,path):
+        json.dump(self.__dict__,open(path,"w"))
+    def loads(self,expand,merge=False):
+        if "sshjob" in expand:
+            if not merge:
+                super(OrderedDict, self).clear()
+            self.job_queues += expand["job_queues"]
+            for label,env in expand["jobs"].items():
+                self[label]=sshjobsys(env["environment"],self.job_queues)
+                self[label].load_from_dict(expand)
+            return True
+        elif "jobs" in expand:
+            print("[SSHJOB] Seems not a sshjobsys dump.")
+            print("[SSHJOB] Use sshjobsys() instead.")
+            return False
+        else:
+            print("[SSHJOB] Seems not a sshjob nor sshjobsys dump.")
+            return False
+
+    def load(self,path=None,merge=False):
+        if path:
+            expand = json.load(open(path,"r"))
+        else:
+            expand = json.load(open(self.file,"r"))
+        if self.loads(expand, merge):
+            if path:
+                self.file=path
+                print("[SSHJOB] Save to",self.file)
+                print("[SSHJOB] For loading, use:  jobs.load(\"%s\")"%self.file)
+            else:
+                self.file=expand["file"]
+
+    def add_job_queue(self,job_queues={}):
+        self.job_queues.update(job_queues)
+        self.show_job_queue()
+
+    def show_job_queue(self):
+        list_job_queues = [name for name,func in self.job_queues.items()]
+        print("[SSHJOB] We recognize %d job queues of : %s"%(len(self.job_queues),", ".join(list_job_queues)))
+
+    def multi_system(self,commandline,system=None,ssh_bash_profile=True):
+        #system = system if system is not None else self.environments[0]
+        #system = system.split(":")
+        pass
--- sshjob/sshjob.py
+++ sshjob/sshjob.py
@@ -20,105 +20,11 @@ from sshjob.shell_runs import *
 from sshjob.job_queues import *
 
 from os.path import expanduser
-#pyraiden_home=expanduser("~")
-pyraiden_base=""
-#pyraiden_log="pyraiden_log/"
+path_base=""
 
 require_files=["header_basic.sh","header_cpu.sh", "header_gpu.sh","footer.sh"]
 
 DEFAULT_JOB_QUEUE={"SGE_DEFAULT":sge_default, "SHELL":shell}
-
-class sshjob(OrderedDict):
-    @staticmethod
-    def version():
-        return '0.1.1'
-    def __init__(self,
-                 envs={"local":":::SHELL"},
-                 job_queues={"SHELL":shell},
-                 file=None):
-        super(sshjob, self).__init__()
-        assert isinstance(envs,dict)
-        assert isinstance(job_queues,dict)
-        for label,env in envs.items():
-            self[label]=sshjobsys(env,job_queues)
-        self.job_queues = job_queues
-        if file:
-            self.load(file)
-        elif file=="":
-            if file:
-                self.load(file)
-                print("[SSHJOB] load from "+self.file)
-            else:
-                date=datetime.datetime.today().strftime("%Y%m%d")[2:]
-                file = "pyjobs"+"."+date
-                for i in range(100):
-                    if not os.path.isfile(file+".%02d"%i):
-                        self.file = file+".%02d"%i
-                        print("[SSHJOB] Save to",self.file)
-                        print("[SSHJOB] For loading, use:  jobs.load(\"%s\")"%self.file)
-                        break
-                else:
-                    raise NameError("[SSHJOB] TOO MANY FILES")
-        self.job_queues = DEFAULT_JOB_QUEUE
-        self.add_job_queue(job_queues)
-
-    @property
-    def __dict__(self):
-        expand={"file":self.file, "sshjob":{}}
-        for label,jobsys in self.items():
-            if isinstance(label,str):
-                expand["sshjob"][label]=jobsys.__dict__
-        return {"sshjob":expand}
-    def show(self,**kwargs):
-        for label,jobsys in self.items():
-            if isinstance(label,str):
-                print("# "+label)
-                jobsys.show(**kwargs)
-    def qsub(self,env,**kwargs):
-        if env not in self:
-            print("env="+env+" not found.")
-        else:
-            self[env].qsub(**kwargs)
-
-    def dump(self,path):
-        json.dump(self.__dict__(),open(path,"w"))
-    def loads(self,expand,merge=False):
-        if "sshjob" in expand:
-            if not merge:
-                super(OrderedDict, self).clear()
-            self.job_queues += expand["job_queues"]
-            for label,env in expand["jobs"].items():
-                self[label]=sshjobsys(env["environment"],self.job_queues)
-                self[label].load_from_dict(expand)
-            return True
-        elif "jobs" in expand:
-            print("[SSHJOB] Seems not a sshjobsys dump.")
-            print("[SSHJOB] Use sshjobsys() instead.")
-            return False
-        else:
-            print("[SSHJOB] Seems not a sshjob nor sshjobsys dump.")
-            return False
-
-    def load(self,path=None,merge=False):
-        if path:
-            expand = json.load(open(path,"r"))
-        else:
-            expand = json.load(open(self.file,"r"))
-        if self.loads(expand, merge):
-            if path:
-                self.file=path
-                print("[SSHJOB] Save to",self.file)
-                print("[SSHJOB] For loading, use:  jobs.load(\"%s\")"%self.file)
-            else:
-                self.file=expand["file"]
-
-    def add_job_queue(self,job_queues={}):
-        self.job_queues.update(job_queues)
-        self.show_job_queue()
-
-    def show_job_queue(self):
-        list_job_queues = [name for name,func in self.job_queues.items()]
-        print("[SSHJOB] We recognize %d job queues of : %s"%(len(self.job_queues),", ".join(list_job_queues)))
 
 
 class sshjobsys(OrderedDict):
@@ -334,12 +240,12 @@ class sshjobsys(OrderedDict):
             print("opt_er",opt_er)
         header = None
         if "#localhost" in jc:
-            header= open(pyraiden_base+"header_basic.sh","r").read()
+            header= open(path_base+"header_basic.sh","r").read()
         else:
-            header= open(pyraiden_base+"header_gpu.sh","r").read() if "gpu" in opt_er or "gs" in opt_er \
-                else open(pyraiden_base+"header_cpu.sh","r").read()
+            header= open(path_base+"header_gpu.sh","r").read() if "gpu" in opt_er or "gs" in opt_er \
+                else open(path_base+"header_cpu.sh","r").read()
         header = header.replace("__DOCKER__INFO__",docker)
-        footer=open(pyraiden_base+"footer.sh","r").read()
+        footer=open(path_base+"footer.sh","r").read()
         if type(com)==str:
             com=[com]
         now = datetime.datetime.now().isoformat()
@@ -462,9 +368,8 @@ class sshjobsys(OrderedDict):
                     print("[SSHJOB] jobs[JOB_ID (int)] = pyjob(**%s)"%repr)
         self.dump()
 
-    def shell_run(self,commandline,system=None,ssh_bash_profile=True):
-        system = system if system is not None else self.environments[0]
-        system = system.split(":")
+    def shell_run(self,commandline,ssh_bash_profile=True):
+        system = self.environment.split(":")
         ssh    = None if len(system[0])==0 else system[0]
         sshdir = None if len(system[1])==0 else system[1]
         return shell_run(commandline,server=ssh,cd=sshdir,ssh_bash_profile=ssh_bash_profile)
@@ -496,11 +401,11 @@ class sshjobsys(OrderedDict):
          head -n %d %s ;
          echo -----------------------[SSHJOB]------------------------------- ;
          tail -n %d %s """%(fne,line,fne,line,fne,fno,line,fno,line,fno)
-        res = self.shell_run(com,system=system,ssh_bash_profile=False)
+        res = self.shell_run(com,ssh_bash_profile=False)
         print(res["stdout"])
 
-    def qstat(self,system=None,depth=5):
-        qstat = self.shell_run(["qstat"], system=system)
+    def qstat(self,depth=5):
+        qstat = self.shell_run(["qstat"], system=self.system)
         qlines = qstat["stdout"].strip().split("\n")
         qlines = [line.strip().split() for line in qlines[2:]]
         jobs={}
@@ -520,8 +425,8 @@ class sshjobsys(OrderedDict):
                     print(len(qline))
         return jobs
 
-    def updating(self,system=None,depth=0,shellsystem=False):
-        system = system if system is not None else self.environments[0]
+    def updating(self,depth=0,shellsystem=False):
+        system = self.environment
 
         wait=[]
         fail_to_run=[]
@@ -671,12 +576,14 @@ class sshjobsys(OrderedDict):
             res=shell_run("""mkdir -p %s"""%sshdir)
             self.check_env(system=system)
             return res
+pyjobs = sshjobsys
 
 class pyjob(dict):
     def __init__(self,jobid,jobname,
                  jobfile="",
                  state="",jc="",qsub="",startat="",git="",
                  pid=None):
+
 
         # jobid : name of shellfile
         # jobname : name of shellfile
@@ -692,7 +599,10 @@ class pyjob(dict):
                           "jobfile":jobfile,
                           "pid":str(pid) if pid is not None else None,
                                  })
-# qsub
+
+    @property
+    def __dict__(self):
+        return self
 
 def parse_qsub_output(res):
     if type(res)!=dict:
