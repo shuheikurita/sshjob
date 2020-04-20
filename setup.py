e79bd6d303b342d9d2cc725ce752c07e918a2c08
Shuhe Kurita <kurita@nlp.ist.i.kyoto-u.ac.jp>
2020-04-20 19:32:16 -0400

    0.0.dev23

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ version = next((line.split('=')[1].strip().replace("'", '')
                                               'sshjob',
                                               '__init__.py'))
                 if line.startswith('__version__ = ')),
-               "0.0.dev22")
+               "0.0.dev23")
 #'0.1.0.dev0')
 
 setup(
